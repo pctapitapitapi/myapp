ccbae5b65e6ce48735904054d9636eaa238d5ebe
Emi Yamamoto <emi.yamamoto73@gmail.com>
2019-07-31 12:28:12 +0900

    setup migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :students, controllers: {
+    sessions:      'students/sessions',
+    passwords:     'students/passwords',
+    registrations: 'students/registrations'
+  }
+  devise_for :teachers, controllers: {
+    sessions:      'teachers/sessions',
+    passwords:     'teachers/passwords',
+    registrations: 'teachers/registrations'
+  }
 end
